d2a653a9e65bac1f862e2a387bc22cad1f79075d
juntong20XX <juntong_2021@outlook.com>
2025-02-12 22:49:34 +0000

    参数写反了, 然后变量名写错了

--- src/__main__.py
+++ src/__main__.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     print("input your Serial path:")
     path = input("[/dev/ttyACM0]:")
 
-    command_like = []
+    command_list = []
 
-    thread = Thread(target=main, args=(path, command_like), daemon=True)
+    thread = Thread(target=main, args=(command_list, path), daemon=True)
 
     thread.start()
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         deg = input("a number for moving the servo, other for exit:")
         if deg.isdigit():
             print("update:", deg)
-            command_like.append((0x0131, int(deg)))
+            command_list.append((0x0131, int(deg)))
         else:
             print("exit")
             exit(0)
