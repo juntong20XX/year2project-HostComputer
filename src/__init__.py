3ed36a5f86844d08a4de66defea0fee2a2d31fe0
juntong20XX <juntong_2021@outlook.com>
2025-02-12 23:07:19 +0000

    添加了 debug 参数

--- src/__init__.py
+++ src/__init__.py
@@ -16,30 +16,41 @@ def get_msg(s: stf.SerialTransfer) -> tuple[int, str] | bool:
     return msg_type, data
 
 
-def match_and_call(s: stf.SerialTransfer, command_list: list[(int, str)]):
+def match_and_call(s: stf.SerialTransfer, command_list: list[(int, str)], debug=False) -> tuple[int, str] | bool:
     """
     获取输入, 然后根据输入
     :param s: SerialTransfer
     :param command_list:
+    :param debug: 
     :return:
     """
     if not (msg := get_msg(s)):
         return False
     msg_type, data = msg
+    if debug:
+        print(time.time(), "before match:", f"msg_type: {msg_type}, data: {data}")
     match msg_type:
         case 0x0001:
             # ping
+            if debug:
+                print(time.time(), f"[{msg_type}]", "matched ping")
             pingloop.ping_call(msg_type, data, s)
             time.sleep(0.3)
         case 0x0011:
             # wait
+            if debug:
+                print(time.time(), f"[{msg_type}]", "matched ping")
             if not command_list:
                 # 指令列表为空, 直接成功
+                if debug:
+                    print(time.time(), "command list is empty, exit")
                 return True
             else:
                 msg_type, data = command_list.pop(0)
                 # 发送指令
-                success = commander.command(s, msg_type, data)
+                if debug:
+                    print(time.time(), "send command", f"command_code {msg_type}, data: {data}")
+                success = commander.command(s, msg_type, data, debug=debug)
                 if success is True:
                     return True
                 elif success is False:
@@ -47,11 +58,11 @@ def match_and_call(s: stf.SerialTransfer, command_list: list[(int, str)]):
                     command_list.insert(0, (msg_type, data))
 
 
-def main(command_list: list[(int, str)], serial_path='/dev/ttyACM0'):
+def main(command_list: list[(int, str)], serial_path='/dev/ttyACM0', debug=False):
     link = stf.SerialTransfer(serial_path)
     try:
         link.open()
         while True:
-            match_and_call(link, command_list)
+            match_and_call(link, command_list, debug=debug)
     finally:
         link.close()
--- src/commander.py
+++ src/commander.py
@@ -6,12 +6,13 @@ from pySerialTransfer import pySerialTransfer as stf
 import time
 
 
-def command(s: stf.SerialTransfer, code: int, target, wait_times=3) -> bool:
+def command(s: stf.SerialTransfer, code: int, target, wait_times=3, debug=False) -> bool:
     """
     发送指令并检查回放
     :param s:
     :param code:
     :param target:
+    :param debug:  是否打印调试信息
     :param wait_times: 回放等待重试次数
     :return:
     """
@@ -20,6 +21,9 @@ def command(s: stf.SerialTransfer, code: int, target, wait_times=3) -> bool:
     tx_size = s.tx_obj(code, tx_size)
     tx_size = s.tx_obj(target, tx_size)
     s.send(tx_size)
+    if debug:
+        print(time.time(), "command: get", code, target)
+        print("waiting for repeat")
 
     # 等待回放
     ty = type(target)
@@ -29,19 +33,30 @@ def command(s: stf.SerialTransfer, code: int, target, wait_times=3) -> bool:
             continue
         msg_type: int = s.rx_obj(obj_type='i', start_pos=0)
         data: ty = s.rx_obj(obj_type=ty, start_pos=4, obj_byte_size=4)
+        if debug:
+            print(time.time(), "command: get repeat", msg_type, data)
         break
     else:
         # 未得到回放
+        if debug:
+            print(time.time(), "command: NOT get ANY repeat")
         return False
     if not (msg_type == -code and data == target):
         # 回放校验失败
+        if debug:
+            print(time.time(), "command: repeat check UNSUCCESSFULLY, want",
+                  f"({-code}, {data}) get", (msg_type, data))
         return False
     # 发送校验成功消息
+    if debug:
+        print(time.time(), "command: repeat check SUCCESSFULLY")
     tx_size = 0
     tx_size = s.tx_obj(0x0012, tx_size)
     tx_size = s.tx_obj(0, tx_size)
     s.send(tx_size)
     # 等待执行结果
+    if debug:
+        print(time.time(), "command: waiting for execution results")
     t = time.time()
     while time.time() - t < 0.3 * wait_times:
         time.sleep(0.2)
@@ -51,22 +66,29 @@ def command(s: stf.SerialTransfer, code: int, target, wait_times=3) -> bool:
         data: ty = s.rx_obj(obj_type=ty, start_pos=4, obj_byte_size=4)
         if msg_type == 0x0014:
             # 执行成功
+            if debug:
+                print(time.time(), "command: waiting for execution successfully")
             return True
         else:
             # 执行失败的代码是 0x0013 XXX: 应该有收到未知代码的解决方案
+            if debug:
+                print(time.time(), "command: waiting for execution results UNSUCCESSFULLY, get code", msg_type)
             return False
     # 超时喽
+    if debug:
+        print(time.time(), "command: waiting for execution results UNSUCCESSFULLY, TIMEOUT")
     return False
 
 
-def servo_move(s: stf.SerialTransfer, target: int) -> bool:
+def servo_move(s: stf.SerialTransfer, target: int, debug=False) -> bool:
     """
     控制电机
     指令代码是 0x0131
     :param s:
     :param target: 舵机的目标度数, 应该在 0~180 之间
+    :param debug:
     :return: bool 成功或失败
     :raise : ValueError: target not in [0, 180]
     """
     assert 0 <= target <= 180, ValueError('target must be between 0 and 180, get', target)
-    return command(s, 0x0131, target)
+    return command(s, 0x0131, target, debug=debug)
